d46cca6981883c022e2c1a7365a58701a227a880
ashoordoushow <ashoor12323@gmail.com>
2025-03-03 12:52:18 -0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { TasksPage } from "./TasksPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <TasksPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/TasksPage.jsx
@@ -0,0 +1,7 @@
+export function TasksPage() {
+  return (
+    <main>
+      <h1>Welcome to React!</h1>
+    </main>
+  )
+}
